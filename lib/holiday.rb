c2016a8759214f31ca4476722ad2988e78a8ba44
Alex  Contreras <alex.code29@gmail.com>
2020-08-30 16:29:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,7 +48,14 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday
+    holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+      puts "  #{holiday.to_s.split('_').map(&:capitalize).join(' ')}: #{supplies.join(', ')}"
+    end
+  end
+
+end
     
     
 end
